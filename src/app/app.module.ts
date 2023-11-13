b900a34487c3db366242b4074ce3c5e7a8d69407
Bo <bbonning42@gmail.com>
2023-11-13 16:16:31 -0600

    game page functionality added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
+import { GameComponent } from './game/game.component';
+import { ArtistCardComponent } from './game/artist-card/artist-card.component';
 
 const routes: Routes = [{ path: "", component: HomeComponent }];
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, GameComponent, ArtistCardComponent],
   imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
