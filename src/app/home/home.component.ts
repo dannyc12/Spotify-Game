d11784ccd2efdefe5a1eb275acab482d9801d304
Danny <94017099+dannyc12@users.noreply.github.com>
2023-11-14 08:24:02 -0600

    implemented gameService feature to save state on 'startGame()'

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import fetchFromSpotify, { request } from "../../services/api";
-import Song from "../models/song";
+import Track from "../models/track";
 import Artist from "../models/artist";
+import { GameService } from "src/services/game.service";
+import { Router } from "@angular/router";
 
 const AUTH_ENDPOINT =
   "https://nuod0t2zoe.execute-api.us-east-2.amazonaws.com/FT-Classroom/spotify-auth-token";
@@ -13,15 +15,31 @@ const TOKEN_KEY = "whos-who-access-token";
   styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  constructor(private gameService: GameService, private router: Router) {}
 
   genres: String[] = ["House", "Alternative", "J-Rock", "R&B"];
   selectedGenre: String = "";
   authLoading: boolean = false;
   configLoading: boolean = false;
   token: String = "";
-  numberOfSongs: number = 1;
+  numberOfTracks: number = 1;
   numberOfArtists: number = 2;
+
+  // MOCK RESPONSE
+  // mockResponse = {
+  //   "artists": [
+  //     {
+  //       "images: [
+  //         {
+  //           "url": "https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228",
+  //           "height": 300,
+  //           "width": 300
+  //         }
+  //       ],
+
+  //     }
+  //   ]
+  // }
 
   ngOnInit(): void {
     this.authLoading = true;
@@ -33,7 +51,7 @@ export class HomeComponent implements OnInit {
         console.log("Token found in localstorage");
         this.authLoading = false;
         this.token = storedToken.value;
-        this.loadGenres(storedToken.value);
+        // this.loadGenres(storedToken.value);
         return;
       }
     }
@@ -62,18 +80,31 @@ export class HomeComponent implements OnInit {
     this.configLoading = false;
   };
 
-  async getTracks() {
-    console.log("trying to get tracks...")
-    const customParams = {'seed_genres': this.selectedGenre, 'limit': this.numberOfSongs}
-    const response = await fetchFromSpotify({
-      token: this.token,
-      endpoint: "recommendations/",
-      params: customParams
+  // // NEEDS TESTING
+  // async getTracks() {
+  //   console.log("trying to get tracks...")
+  //   const customParams = {'seed_genres': this.selectedGenre, 'limit': this.numberOfTracks}
+  //   const response = await fetchFromSpotify({
+  //     token: this.token,
+  //     endpoint: "recommendations/",
+  //     params: customParams
+  //   });
+  //   console.log("Tracks:  " + JSON.stringify(response))
+  //   console.log(response.tracks[0])
+  //   console.log(response.tracks[1])
+  //   this.mapResponseToTracks(response.tracks[0])
+  // }
+
+  startGame() {
+    // save the state before routing to game component
+    this.gameService.setGameState({
+      genre: this.selectedGenre,
+      numberOfTracks: this.numberOfTracks,
+      numberOfArtists: this.numberOfArtists
     });
-    console.log("Tracks:  " + JSON.stringify(response))
-    console.log(response.tracks[0])
-    console.log(response.tracks[1])
-    this.mapResponseToSongs(response.tracks[0])
+    // user router here to ensure that our state is saved BEFORE we move to the game component
+    console.log(`saved state: ${JSON.stringify(this.gameService.getGameState())}`)
+    // this.router.navigate(['/game']);
   }
 
   setGenre(selectedGenre: any) {
@@ -82,9 +113,9 @@ export class HomeComponent implements OnInit {
     console.log(TOKEN_KEY);
   }
 
-  setNumberOfSongs(selectedNumber: any) {
-    this.numberOfSongs = selectedNumber;
-    console.log("User chose " + this.numberOfSongs + " songs.")
+  setNumberOfTracks(selectedNumber: any) {
+    this.numberOfTracks = selectedNumber;
+    console.log("User chose " + this.numberOfTracks + " Tracks.")
   }
 
   setNumberOfArtists(selectedNumber: any) {
@@ -92,21 +123,24 @@ export class HomeComponent implements OnInit {
     console.log("User chose " + this.numberOfArtists + " artists per question.")
   }
 
-  mapResponseToSongs(trackResponse: any): Song {
-    let songArtist: Artist = {
+  // NEEDS TESTING
+  mapResponseToTracks(trackResponse: any): Track {
+    let TrackArtist: Artist = {
+      id: trackResponse.artists[0].id,
       name: trackResponse.artists[0].name,
       imgUrl: trackResponse.artists[0].images[0].url
     }
-    let newSong: Song = {
+    let newTrack: Track = {
+      id: trackResponse.id,
       name: trackResponse.name,
-      artist: songArtist,
+      // artists: [TrackArtist],
       previewUrl: trackResponse.preview_url,
       detailsUrl: trackResponse.href
     }
-    for (let key in newSong) {
+    for (let key in newTrack) {
       console.log(`${key}: ${key.valueOf}`)
     }
-    return newSong;
+    return newTrack;
   }
 
   
